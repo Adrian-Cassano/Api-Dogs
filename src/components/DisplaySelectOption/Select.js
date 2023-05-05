a75bac667506c7bbad678ecf6e54001ed027fd11
Adrian-Cassano <adriandcassano@hotmail.com>
2023-05-05 13:46:50 -0300

    feat: add select filter for dogs breeds and navigate to landing when api is not loaded

--- src/components/DisplaySelectOption/Select.js
+++ src/components/DisplaySelectOption/Select.js
@@ -1,12 +1,25 @@
 import React from "react";
+
+import BREEDS from "../../components/Array/Array";
+
 import styles from "./Select.module.css";
 
+
+
 const Select = () => {
+
   return (
     <div id={styles.MasterContainer}>
-      <select id={styles.Select}>
-        <option>All dogs</option>
-        <option>Small</option>
+      <select
+        id={styles.Select}
+      >
+        {BREEDS?.map((dog) => {
+          return (
+            <option value={dog.breeds} key={dog.key}>
+              {dog.breeds}
+            </option>
+          );
+        })}
       </select>
     </div>
   );
