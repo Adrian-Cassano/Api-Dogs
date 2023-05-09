7fc2fb9a724c8b6e196156458ca712020b8a98fb
Adrian-Cassano <adriandcassano@hotmail.com>
2023-05-09 16:48:41 -0300

    feat:add filter to age and temperament with array

---
+++ src/components/Array/ArrayAge.js
@@ -0,0 +1,15 @@
+
+const AGES = [
+    {age:"All", key:1},
+    {age:10,key:2},
+    {age:11,key:3},
+    {age:12,key:4},
+    {age:13,key:5},
+    {age:14,key:6},
+    {age:15,key:7},
+    {age:16,key:8},
+    {age:17,key:9},
+    {age:18,key:10},
+    {age:20,key:11},
+] 
+export default AGES
